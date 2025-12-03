cce65397951a4221b4b670a6e9679510d0485815
Chandushree-BM <chandushreebm2002@gmail.com>
2025-12-03 09:29:48 +0530

    .

---
+++ EvenOddCheck.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class EvenOddCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();  // user enters a number
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is Even");
+        } else {
+            System.out.println(number + " is Odd");
+        }
+
+        scanner.close();
+    }
+}
