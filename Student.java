3df6298dad28dff9152ebdc357cc3b83bb1cd8c0
Chandushree-BM <chandushreebm2002@gmail.com>
2025-12-03 09:21:50 +0530

    first commit

---
+++ Student.java
@@ -0,0 +1,9 @@
+public class Student{
+    public static void main(String[] args){
+        String name;
+        name="chandu";
+        int age=25;
+        System.out.println("Student name:"+name);
+        System.out.println("Student age:"+age);
+    }
+}
